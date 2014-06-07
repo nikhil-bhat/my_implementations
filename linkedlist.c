97b574309aa08ebb03476464027db1c21ee6c52b
nik <nikhilbhat2008@gmail.com>
2014-06-07 15:52:46 +0530

    added the linked list boiler plate code

---
+++ linkedlist.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node 
+{
+struct node * next;
+int data;
+}node;
+int main()
+{
+node * head=NULL ;
+head=(node *)malloc(sizeof(node));
+head->next=NULL;
+head->data=8;
+printf("Head stores:%d\n",head->data);
+
+return 0;
+
+
+}
