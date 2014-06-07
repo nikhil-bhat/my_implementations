d219f0e1566fc4f74535452df47988df87a9edba
nik <nikhilbhat2008@gmail.com>
2014-06-07 15:55:44 +0530

    used Astyle

--- linkedlist.c
+++ linkedlist.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct node 
+typedef struct node
 {
-struct node * next;
-int data;
-}node;
+    struct node * next;
+    int data;
+} node;
 int main()
 {
-node * head=NULL ;
-head=(node *)malloc(sizeof(node));
-head->next=NULL;
-head->data=8;
-printf("Head stores:%d\n",head->data);
+    node * head=NULL ;
+    head=(node *)malloc(sizeof(node));
+    head->next=NULL;
+    head->data=8;
+    printf("Head stores:%d\n",head->data);
 
-return 0;
+    return 0;
 
 
 }
