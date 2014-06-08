8f72288eabb1b9a02b3706e799bbffa872a0508f
nik <nikhilbhat2008@gmail.com>
2014-06-08 11:35:02 +0530

    Added insert function in linked list

--- linkedlist.c
+++ linkedlist.c
@@ -5,14 +5,38 @@ typedef struct node
     struct node * next;
     int data;
 } node;
+node * head=NULL ;
+
+void insert(int data);
+void insert(int data) {
+    node * temp=malloc(sizeof(node));
+    temp->data=data;
+    temp->next=NULL;
+    head->next=temp;
+    head=temp;
+
+}
+
+
+
 int main()
-{
-    node * head=NULL ;
+{ node * xul;
+
     head=(node *)malloc(sizeof(node));
     head->next=NULL;
     head->data=8;
+xul=head;
     printf("Head stores:%d\n",head->data);
+    insert(5);
+    insert(-1);
 
+
+    while(xul!=NULL)
+    {
+        printf("Data:%d\n",xul->data);
+        xul=xul->next;
+
+    }
     return 0;
 
 
---
+++ mergesort.py
@@ -0,0 +1,5 @@
+def mergesort(list):
+	for e in list:
+	 print e
+list=[1,2,3,4]
+mergesort(list)
